8d74d5036cb392b7f341e9b04a0baaa45c5bda54
lukem512 <hi@lukemitchell.co>
2019-07-16 18:43:29 +0100

    Add simple black/whitelist functionality

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const freecycle = require('./freecycle')
 
+const blacklist = ['washing', 'iron'].map(item => item.toLowerCase())
+const whitelist = ['bed'].map(item => item.toLowerCase())
+
 // Testing function
 freecycle.getPosts('CambridgeUK', (err, posts) => {
   if (err) {
     console.error('Could not retrieve posts', err)
     process.exit(1)
   }
-  console.log(posts)
+
+  // Remove any posts with blacklisted titles
+  const filtered = posts.filter(post => {
+    for (item of blacklist) {
+      if (post.title.toLowerCase().indexOf(item) > -1) {
+        return false
+      }
+    }
+    return true
+  })
+
+  // Look for highlighted posts (titles with whitelisted terms)
+  const highlights = posts.filter(post => {
+    for (item of whitelist) {
+      if (post.title.toLowerCase().indexOf(item) > -1) {
+        return true
+      }
+    }
+    return false
+  })
+
+  console.log('highlights', highlights)
   process.exit(0)
 })
