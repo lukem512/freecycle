babc0a028d99cfc108454a242ec71db366353cbf
lukem512 <hi@lukemitchell.co>
2019-07-17 22:58:42 +0100

    Improve example

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const async = require('async')
+const moment = require('moment')
 const Group = require('./freecycle/group')
 
 const blacklist = ['washing', 'iron'].map(item => item.toLowerCase())
@@ -7,31 +9,48 @@ const GROUPS = ['CambridgeUK', 'IslingtonEastUK']
 const groups = GROUPS.map(group => new Group(group))
 
 // Testing function
-groups.forEach(group => group.getPosts((err, posts) => {
+const results = []
+async.forEachOf(groups, (group, key, cb) => {
+  group.getPosts((err, posts) => {
+    if (err) {
+      return cb(err)
+    }
+
+    // Remove any posts with blacklisted titles
+    const filtered = posts.filter(post => {
+      for (item of blacklist) {
+        if (post.title.toLowerCase().indexOf(item) > -1) {
+          return false
+        }
+      }
+      return true
+    })
+
+    // Look for highlighted posts (titles with whitelisted terms)
+    const highlights = posts.filter(post => {
+      for (item of whitelist) {
+        if (post.title.toLowerCase().indexOf(item) > -1) {
+          return true
+        }
+      }
+      return false
+    })
+
+    posts.forEach(post => results.push(post))
+    cb()
+  })
+}, err => {
   if (err) {
-    console.error('Could not retrieve posts', err)
+    console.error('Could not retrieve posts: ', err)
     process.exit(1)
   }
 
-  // Remove any posts with blacklisted titles
-  const filtered = posts.filter(post => {
-    for (item of blacklist) {
-      if (post.title.toLowerCase().indexOf(item) > -1) {
-        return false
-      }
-    }
-    return true
+  // Sort ascending by date
+  const sorted = results.sort((a, b) => {
+    const dateA = new moment(a.date)
+    const dateB = new moment(b.date)
+    return dateA.diff(dateB)
   })
 
-  // Look for highlighted posts (titles with whitelisted terms)
-  const highlights = posts.filter(post => {
-    for (item of whitelist) {
-      if (post.title.toLowerCase().indexOf(item) > -1) {
-        return true
-      }
-    }
-    return false
-  })
-
-  console.log(posts)
-}))
+  console.log(sorted)
+})
